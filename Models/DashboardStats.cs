ba4268a714afe22739f772429afca150c65d9e5e
Fex11 <AndrianiainaLiantsoa11@gmail.com>
2025-12-19 11:55:21 +0300

    Delete Models

--- Models/DashboardStats.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Backoffice_bibliotheque.Models
-{
-    public class DashboardStats
-    {
-        public int ActiveLoans { get; set; }
-        public int LateLoans { get; set; }
-        public int ExpiredReservations { get; set; }
-    }
-}
